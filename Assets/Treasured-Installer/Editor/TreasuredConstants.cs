33982e763753089b8c3b8e06dd488024c3818db9
Jalpen <djalpen24@gmail.com>
2022-09-23 15:30:27 -0400

    fix: fixed version control package issue

---
+++ Assets/Treasured-Installer/Editor/TreasuredConstants.cs
@@ -0,0 +1,16 @@
+namespace Treasured.UnitySdk.Installer
+{
+    public class TreasuredConstants
+    {
+        public const string TreasuredInstallerLogPrefix = "<b>[Treasured Installer]</b>: ";
+        public const string TreasuredUnInstallerLogPrefix = "<b>[Treasured UnInstaller]</b>: ";
+        public const string TreasuredInstallerKey = "TreasuredInstallerInProcess";
+        public const string VersionControlPackageName = "com.unity.collab-proxy";
+        public const string NewtonsoftJsonPackageName = "com.unity.nuget.newtonsoft-json";
+        public const string UnityMeshSimplifierPackageName = "com.whinarn.unitymeshsimplifier";
+        public const string UnityMeshSimplifierGitSource = "https://github.com/Whinarn/UnityMeshSimplifier.git";
+        public const string TreasuredUnitySdkPackageName = "com.treasured.unitysdk";
+        public const string TreasuredUnitySdkGitSource =
+            "https://github.com/TB-Terence/treasured-sdk-for-unity.git#upm";
+    }
+}
